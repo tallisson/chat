9e0882136b13cb0e4acf2450cf732941fa488950
Thiago Allisson <allissonribeiro02@gmail.com>
2020-10-11 23:33:26 -0300

    Adicionando port ao server

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+//app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
 
+const port = process.env.PORT || 8080;
 let messages = [];
 
 app.use('/', (req, res) => {
-  res.render('index.html');
+  res.render('index', { port: port });
 });
 
 io.on('connection', socket => {
@@ -27,7 +29,6 @@ io.on('connection', socket => {
   });
 });
 
-const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log(`Server running in ${port}`);
 });
