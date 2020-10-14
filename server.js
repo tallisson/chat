517461b08492dc89161750f767d367566f3cbf18
Thiago Allisson <allissonribeiro02@gmail.com>
2020-10-14 20:52:56 -0300

    modificando arquivos base

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
+/* 
+ * Bibliotecas Node JS utilizadas:
+ * 1) Express -> criar um servidor web (http) e o servidor do chat
+ * 2) Path -> diretórios do projeto
+ */
 const express = require('express');
 const path = require('path');
 
+// Criamos o servidor http
 const app = express();
 const server = require('http').createServer(app);
+// Definindo o servidor do chat (WSS). Baseado em WebSocket (WSS)
 const io = require('socket.io')(server);
 
+// Configurando a pasta onde vão ser encontrados os arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
-//app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }))
+// Configurar que estamos construindo telas em HTML
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
+// Definindo a porta onde roda o servidor
 const port = process.env.PORT || 8080;
-let messages = [];
+// Lista (Array) de mensagens do chat
+let mensagens = [];
 
-app.use('/', (req, res) => {
-  res.render('index', { port: port });
+// Definindo as rotas (urls) acessíveis no servidor
+app.use('/', function(req, res) {
+  res.render('index.html');
 });
 
-io.on('connection', socket => {
-  console.log(`Socket conectado: ${socket.id}`);
+// Responsável por gerenciar as conexões dos clientes
+io.on('connection', function(socket) {
+  console.log('Socket conectado: ' + socket.id);
 
-  socket.emit('previousMessages', messages);
+  socket.emit('mensagensAnteriores', mensagens);
 
-  socket.on('sendMessage', data => {
-    messages.push(data);
-    socket.broadcast.emit('receivedMessage', data);
+  socket.on('enviarMensagem', function(dados) {
+    mensagens.push(dados);
+    socket.broadcast.emit('mensagemRecebida', dados);
   });
 });
 
-server.listen(port, () => {
-  console.log(`Server running in ${port}`);
+server.listen(port, function() {
+  console.log('Server está sendo executado na porta ' + port);
 });
